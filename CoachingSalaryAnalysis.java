e3da0817405f563b60c217b38a2e03c5dfba7ead
jpittignano <jordanpitt@me.com>
2021-11-04 09:30:06 -0400

    Methods outline

---
+++ CoachingSalaryAnalysis.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class CoachingSalaryAnalysis{
+
+  public static void main (String[] args) throws FileNotFoundException{
+  }
+
+  //find the conference that has the highest average salaries
+  public static String bestConference(){
+  }
+
+  //find the number of schools that are in the top fifty size that have coaches that makes over 1 million
+  public static int numSchoolsTopFiftySizeOverMil(){
+  }
+
+  //find the number of schools that are in the top fifty academic that have coaches that makes over 1 million
+  public static int numSchoolsTopFiftyAcademicOverMil(){
+  }
+
+  //finds the average salary reduction for Texas schools
+  public static int avgCovidReductionTexas(){
+  }
+
+  //finds the average salary reduction for other schools
+  public static int avgCovidReductionOther(){
+  }
+
+
+  //compare which has a larger correlation to salary amount, size or academics
+  public static String compareSizeAndAcademics(){
+
+  }
+
+  //compare average salary reduction due to COVID in texas schools versus every other schools
+  public static String compareTexasAndOthers(){
+
+  }
